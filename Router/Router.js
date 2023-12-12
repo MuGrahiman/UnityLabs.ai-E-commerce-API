693284ae671613c3bf335be26e6d6bedc5046a06
MuGrahiman <mujeebrahiman2000@gmail.com>
2023-12-12 23:31:29 +0530

    router protected

--- Router/Router.js
+++ Router/Router.js
@@ -9,6 +9,7 @@ import {
   getSellerCatalog,
 } from "../Controller/CatelogController";
 import { createOrder, getSellerOrders } from "../Controller/OrderController";
+import auth from "../Middleware/Auth";
 const router = express.Router();
 
 router.post("/auth/register", register);
@@ -16,9 +17,9 @@ router.post("/auth/login", login);
 
 router.get("/buyer/list-of-sellers", getListOfSellers);
 router.get("/buyer/seller-catalog/:seller_id", getSellerCatalog);
-router.post("/buyer/create-order/:seller_id", createOrder);
+router.post("/buyer/create-order/:seller_id", auth, createOrder);
 
-router.post("/seller/create-catalog", createCatalog);
+router.post("/seller/create-catalog", auth, createCatalog);
 router.get("/seller/orders", getSellerOrders);
 
 export default router;
