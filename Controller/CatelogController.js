20773bad4223f7fa81690fb0081465770a40299a
MuGrahiman <mujeebrahiman2000@gmail.com>
2023-12-13 15:56:25 +0530

    modified create catelog controller

--- Controller/CatelogController.js
+++ Controller/CatelogController.js
@@ -1,17 +1,16 @@
-import Catalog from '../Models/Catelog-Model.js';
-import Product from '../Models/Product-Model.js';
-import User from '../Models/User-Model.js';
+import { saveToCatalog } from "../Helpers/Catalog-Helper.js";
+import Catalog from "../Models/Catelog-Model.js";
+import Product from "../Models/Product-Model.js";
+import User from "../Models/User-Model.js";
 
 export const createCatalog = async (req, res) => {
-  const sellerId = req.userId; 
+  const sellerId = req.userId;
   const { items } = req.body;
-  console.log(req.body);
 
   try {
     const seller = await User.findById(sellerId);
-    console.log(seller);
-    if (!seller || seller.type !== 'seller') return res.status(401).json({ message: 'Unauthorized' });
-    
+    if (!seller || seller.type !== "seller")
+      return res.status(401).json({ message: "Unauthorized" });
 
     const productIds = [];
 
@@ -24,33 +23,31 @@ export const createCatalog = async (req, res) => {
       productIds.push(savedProduct._id);
     }
 
-    const newCatalog = new Catalog({
-      seller: sellerId,
-      products: productIds,
-    });
-
-    const savedCatalog = await newCatalog.save();
-
+    const savedCatalog = await saveToCatalog(sellerId, productIds);
     res.status(201).json(savedCatalog);
   } catch (error) {
-    res.status(500).json({ message: error.message || 'Server issue' });
+    res.status(500).json({ message: error.message || "Server issue" });
   }
 };
 
 export const getSellerCatalog = async (req, res) => {
-  const buyerId = req.userId; 
-  const sellerId = req.params.sellerId; 
+  const buyerId = req.userId;
+  const sellerId = req.params.sellerId;
 
   try {
     const seller = await User.findById(sellerId);
-    if (!seller || seller.type !== 'seller') {
-      return res.status(404).json({ message: 'Seller not found' });
+    if (!seller || seller.type !== "seller") {
+      return res.status(404).json({ message: "Seller not found" });
     }
 
-    const catalog = await Catalog.findOne({ seller: sellerId }).populate('products');
+    const catalog = await Catalog.findOne({ seller: sellerId }).populate(
+      "products"
+    );
 
     if (!catalog) {
-      return res.status(404).json({ message: 'Catalog not found for the seller' });
+      return res
+        .status(404)
+        .json({ message: "Catalog not found for the seller" });
     }
 
     const catalogInfo = {
@@ -67,6 +64,6 @@ export const getSellerCatalog = async (req, res) => {
 
     res.status(200).json(catalogInfo);
   } catch (error) {
-    res.status(500).json({ message: error.message || 'Server issue' });
+    res.status(500).json({ message: error.message || "Server issue" });
   }
 };
